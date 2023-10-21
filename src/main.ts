565720dc968bab37d951ad4b2111aa59576704de
alexander <lokistapway@yandex.ru>
2023-10-21 22:56:59 +0300

    Поправлено подключение пинии

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,7 @@ import App from './App.vue'
 import {createPinia} from "pinia";
 
 const pinia = createPinia()
-const app = createApp(App)
 
-app.use(pinia)
-
-createApp(App).mount('#app')
+createApp(App)
+    .use(pinia)
+    .mount('#app')
